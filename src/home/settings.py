b2e8dbb75a99740a122a3db4fe58597b46808e77
deepmonapara9 <deepmonapara0@gmail.com>
2024-07-18 23:31:04 +0530

    Change

--- src/home/settings.py
+++ src/home/settings.py
@@ -97,7 +97,7 @@ if DATABASE_URL is not None:
         'default': dj_database_url.config(
                 default=DATABASE_URL,
                 conn_max_age=CONN_MAX_AGE,
-                conn_health_checks=True
+                conn_health_checks=True,
             )
 }
 
