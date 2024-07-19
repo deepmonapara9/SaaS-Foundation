738417bf060cb643f23794186cd42bb576321c33
deepmonapara9 <deepmonapara0@gmail.com>
2024-07-19 23:33:54 +0530

    Add simple Js and Css navbar using Flowbite

--- src/home/settings.py
+++ src/home/settings.py
@@ -136,7 +136,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = config('STATIC_URL', default='/static/')
+STATICFILES_BASE_DIR = Path(config('STATICFILES_BASE_DIR', default=BASE_DIR / "staticfiles"))
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR,
+]
+
+# output for python manage.py collectstatic
+# local cdn
+STATIC_ROOT = Path(config('STATIC_ROOT', default=BASE_DIR / "local-cdn"))
+# if not DEBUG:
+#     STATIC_ROOT = BASE_DIR / "prod-cdn"
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
