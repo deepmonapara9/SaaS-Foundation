e02f835fbded070d164c25a9d45ea2117b0a0dd4
deepmonapara9 <deepmonapara0@gmail.com>
2024-07-22 16:14:41 +0530

    Add Login template

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-    context = {'user':user}
+    if request.method == "POST":
+        username =  request.POST["username"]
+        password =  request.POST["password"] 
+        if all([username, password]):  
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print("Login here")
+                return redirect("/")
+    context = {}
     return render(request, "auth/login.html", context)
 
 def register_view(request):
