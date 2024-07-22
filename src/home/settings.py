72a06a7733212543efdcde4fc250c634231933a2
deepmonapara9 <deepmonapara0@gmail.com>
2024-07-22 21:37:49 +0530

    Integrate AllAuth for authentication

--- src/home/settings.py
+++ src/home/settings.py
@@ -69,6 +69,11 @@ INSTALLED_APPS = [
     # my-apps
     "commando",
     "visits",
+    # third-party-apps
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +84,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -138,6 +144,26 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# django allauth config
+LOGIN_REDIRECT_URL='/'
+ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_SUBJECT_PREFIX = "FingerChips "
+ACCOUNT_EMAIL_REQUIRED=True 
+
+AUTHENTICATION_BACKENDS = [
+    #...
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+    #...
+]
+
+# Provider specific settings
+SOCIALACCOUNT_PROVIDERS = {}
 
 
 # Internationalization
