981aca2af2517efff4a895cebda1d8b7f5e4614d
deepmonapara9 <deepmonapara0@gmail.com>
2024-07-24 03:14:40 +0530

    Add links in the navbar

--- src/home/urls.py
+++ src/home/urls.py
@@ -21,7 +21,7 @@ from .views import home_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", home_view), #index page -> root page
+    path("", home_view, name='home'), #index page -> root page
     path("about/", about_view),
     path("login/", auth_views.login_view),
     path("register/", auth_views.register_view),
