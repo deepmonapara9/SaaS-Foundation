c9bbd2cde0e1f0e8d328b88a932a0c7d261c701a
deepmonapara9 <deepmonapara0@gmail.com>
2024-07-26 01:08:12 +0530

    Add only user can see the page

--- src/home/urls.py
+++ src/home/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
-from .views import home_view, about_view, pw_protected_view
+from .views import home_view, about_view, pw_protected_view, user_only_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +29,5 @@ urlpatterns = [
     path("hello-world.html", home_view),
     path('accounts/', include('allauth.urls')),
     path('protected/', pw_protected_view),
+    path('protected/user-only', user_only_view),
 ]
