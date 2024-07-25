24c96bea89028ea64e24952270957408e9bcc42b
deepmonapara9 <deepmonapara0@gmail.com>
2024-07-25 16:06:54 +0530

    Add password protected pages and django sessions

--- src/home/urls.py
+++ src/home/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
-from .views import home_view, about_view
+from .views import home_view, about_view, pw_protected_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +28,5 @@ urlpatterns = [
     path("hello-world/", home_view),
     path("hello-world.html", home_view),
     path('accounts/', include('allauth.urls')),
+    path('protected/', pw_protected_view),
 ]
