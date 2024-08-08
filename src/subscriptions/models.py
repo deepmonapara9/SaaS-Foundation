78b15a005ea5da80ccc779b39a898ca8b705644c
deepmonapara9 <deepmonapara0@gmail.com>
2024-08-09 01:00:31 +0530

    Sync User Subscription with the User

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,24 +1,70 @@
 from django.db import models
 from django.contrib.auth.models import Group, Permission
+from django.conf import settings
+from django.db.models.signals import post_save
 
 # Create your models here.
 
+User = settings.AUTH_USER_MODEL  # auth.User
+
+ALLOW_CUSTOM_GROUPS = True
 SUBSCRIPTION_PERMISSIONS = [
-            ("advanced", "Advanced Perm"), # Subscriptions.advanced
-            ("pro", "Pro Perm"), # Subscriptions.pro
-            ("basic", "Basic Perm"), # Subscriptions.basic
-            ("basic_ai", "Basic AI Perm") # Subscriptions.basic_ai
-        ]
+    ("advanced", "Advanced Perm"),  # Subscriptions.advanced
+    ("pro", "Pro Perm"),  # Subscriptions.pro
+    ("basic", "Basic Perm"),  # Subscriptions.basic
+    ("basic_ai", "Basic AI Perm"),  # Subscriptions.basic_ai
+]
+
 
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
     active = models.BooleanField(default=True)
-    groups = models.ManyToManyField(Group) # one-to-one
-    permissions =  models.ManyToManyField(Permission, limit_choices_to={
-        "content_type__app_label": "subscriptions", 
-        "codename__in": [x[0]for x in SUBSCRIPTION_PERMISSIONS]
-        }
+    groups = models.ManyToManyField(Group)  # one-to-one
+    permissions = models.ManyToManyField(
+        Permission,
+        limit_choices_to={
+            "content_type__app_label": "subscriptions",
+            "codename__in": [x[0] for x in SUBSCRIPTION_PERMISSIONS],
+        },
     )
-    
+
+    def __str__(self):
+        return f"{self.name}"
+
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
+
+
+class UserSubscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(
+        Subscription, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    active = models.BooleanField(default=True)
+
+
+def user_sub_post_save(sender, instance, *args, **kwargs):
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscription
+    groups_ids = []
+    if subscription_obj is not None:
+        groups = subscription_obj.groups.all()
+        groups_ids = groups.values_list('id', flat=True)
+    if not ALLOW_CUSTOM_GROUPS:
+        user.groups.set(groups_ids)
+    else:
+        subs_qs = Subscription.objects.filter(active=True)
+        if subscription_obj is not None:
+            subs_qs = subs_qs.exclude(id=subscription_obj.id)
+        subs_groups = subs_qs.values_list("groups__id", flat=True)
+        subs_groups_set = set(subs_groups)
+        # groups_ids = groups.value_list('id', flat=True) # [1,2,3]
+        current_groups = user.groups.all().values_list("id", flat=True)
+        groups_ids_set = set(groups_ids)
+        current_groups_set = set(current_groups) - subs_groups_set
+        final_groups_set = list(groups_ids_set | current_groups_set)
+        user.groups.set(final_groups_set)
+
+
+post_save.connect(user_sub_post_save, sender=UserSubscription)
