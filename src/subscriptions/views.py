a54382c1ff1fa865d278084e891bffda43e204fe
deepmonapara9 <deepmonapara0@gmail.com>
2024-08-18 03:10:59 +0530

    User can cancel subscription

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -3,6 +3,7 @@ from subscriptions.models import SubscriptionPrice, UserSubscription
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 import helpers.billing
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,12 +21,38 @@ def user_subscription_view(request):
             for k, v in sub_data.items():
                 setattr(user_sub_obj, k, v)
             user_sub_obj.save()
+        messages.success(request, "Your plan has been refreshed.")
         return redirect(user_sub_obj.get_absolute_url())
 
     context = {
         "subscription": user_sub_obj,
     }
     return render(request, "subscriptions/user_detail_view.html", context)
+
+
+@login_required
+def user_subscription_cancel_view(request):
+    user_sub_obj, created = UserSubscription.objects.get_or_create(user=request.user)
+    # sub_data = user_sub_obj.serialize()
+    if request.method == "POST":
+        print("refresh subscription")
+        if user_sub_obj.stripe_id and user_sub_obj.is_active_status:
+            sub_data = helpers.billing.cancel_subscription(
+                user_sub_obj.stripe_id,
+                reason="User wanted to end",
+                feedback="other",
+                raw=False,
+            )
+            for k, v in sub_data.items():
+                setattr(user_sub_obj, k, v)
+            user_sub_obj.save()
+            messages.success(request, "Your plan has been cancelled.")
+        return redirect(user_sub_obj.get_absolute_url())
+
+    context = {
+        "subscription": user_sub_obj,
+    }
+    return render(request, "subscriptions/user_cancel_view.html", context)
 
 
 def subscription_price_view(request, interval="month"):
